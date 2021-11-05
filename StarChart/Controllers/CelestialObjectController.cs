e37de8227dd96b281bfd7415158b5ce9eff2ebf0
stanislav_zadorozhnii@epam.com <stanislav_zadorozhnii@epam.com>
2021-11-05 11:27:20 +0200

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,59 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestial = _context.CelestialObjects.SingleOrDefault(p => p.Id == id);
+
+            if (celestial == null)
+            {
+                return NotFound();
+            }
+
+            PopulateSatellites(celestial);
+
+            return Ok(celestial);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestial = _context
+                .CelestialObjects
+                .Where(p => p.Name == name)
+                .ToList();
+
+            if (celestial.Count == 0)
+            {
+                return NotFound();
+            }
+
+            celestial.ForEach(PopulateSatellites);
+
+            return Ok(celestial);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestial = _context
+                .CelestialObjects
+                .ToList();
+
+            celestial.ForEach(PopulateSatellites);
+
+            return Ok(celestial);
+        }
+
+        private void PopulateSatellites(CelestialObject celestial)
+        {
+            celestial.Satellites = _context
+                .CelestialObjects
+                .Where(p => p.OrbitedObjectId == celestial.Id)
+                .ToList();
         }
     }
 }
